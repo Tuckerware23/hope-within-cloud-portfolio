622a97696e1b9e226dc93089e764e9465e362b3c
Tuckerware23 <159398525+Tuckerware23@users.noreply.github.com>
2026-04-24 05:14:45 -0700

    Add App.jsx component for main view

---
+++ website-deployment/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the Hope Within Universe</h1>
+      <p>This is the beginning of your React and Vite website.</p>
+    </div>
+  );
+}
+
+export default App;
